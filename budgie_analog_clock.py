58e6a96523fd617cbbe32e381bc89c9da164501f
Sam Lane <67085765+samlane-ma@users.noreply.github.com>
2020-09-03 09:23:03 -0400

    Enhancement and a efficiency changes
    Clock dconf settings were reloading on every re-draw.  Now they only reload when changed.
    Setting added to allow toggling the hour markings on the clock face.
    Color selection button for the clock face now correctly indicates when the clock face is set to transparent.

--- budgie_analog_clock.py
+++ budgie_analog_clock.py
@@ -27,18 +27,24 @@ from math import sin, cos, pi
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-IMAGE_SIZE = 100
-X_CENTER = 50
-Y_CENTER = 50
-CLOCK_RADIUS = 37
-HOUR_HAND_LENGTH = 21
-MINUTE_HAND_LENGTH = 31
-UPDATE_INTERVAL = 5
+""" Some "constants":
+    Clock is drawn at 100x100 px resolution, with a center at 50,50 and 
+    then scaled to fit panel
+"""
+IMAGE_SIZE         = 100  # 100 default
+X_CENTER           =  50  #  50 default
+Y_CENTER           =  50  #  50 default
+CLOCK_RADIUS       =  37  #  37 default
+HOUR_HAND_LENGTH   =  21  #  21 default
+MINUTE_HAND_LENGTH =  31  #  31 default
+CLOCK_THICKNESS    =   4  #   4 default
+HAND_THICKNESS     =   6  #   6 default
+UPDATE_INTERVAL    =   5  #   5 default (seconds)
 """ The applet checks the time every UPDATE_INTERVAL seconds. However, the
-    clock only redraws if the minute has changed, so if an immediate update is
-    needed due to a settings change, we set the old minute to -1 to force the
-    update, instead of waiting for the minute to change. The FORCE_UPDATE
-    "constant" is just to clarify the purpose of using "-1" in the code.
+    clock only redraws if the minute has changed, so if an immediate redraw is
+    needed due to a settings change, we set the old minute to -1 to trigger the
+    redraw, instead of waiting for the minute to change. The following 
+    FORCE_UPDATE "constant" is just to clarify using "-1" in the code.
 """
 FORCE_UPDATE = -1
 
@@ -72,20 +78,19 @@ class BudgieAnalogClockSettings(Gtk.Grid):
         self.label_colors = ["Clock Color", "Hands Color", "Face Color"]
         self.setting_name = ["clock-outline", "clock-hands", "clock-face"]
 
-        self.blank_label = Gtk.Label("")
-        self.blank_label.set_halign(Gtk.Align.START)
-        self.attach(self.blank_label, 0, 0, 2, 1)
-        self.label_size = Gtk.Label("Clock Size (px)")
-        self.label_size.set_halign(Gtk.Align.START)
-        self.label_size.set_valign(Gtk.Align.CENTER)
-        self.attach(self.label_size, 0, 1, 1, 1)
+        blank_label = Gtk.Label("")
+        self.attach(blank_label, 0, 0, 2, 1)
+        label_size = Gtk.Label("Clock Size (px)")
+        label_size.set_halign(Gtk.Align.START)
+        label_size.set_valign(Gtk.Align.CENTER)
+        self.attach(label_size, 0, 1, 1, 1)
 
-        self.adj = Gtk.Adjustment(value=app_settings.get_int("clock-size"),
-                                  lower=22, upper=100, step_incr=1)
-        self.spin_clock_size = Gtk.SpinButton()
-        self.spin_clock_size.set_adjustment(self.adj)
-        self.spin_clock_size.set_digits(0)
-        self.attach(self.spin_clock_size, 1, 1, 1, 1)
+        spin_clock_size_adj = Gtk.Adjustment(value=app_settings.get_int("clock-size"),
+                             lower=22, upper=100, step_incr=1)
+        spin_clock_size = Gtk.SpinButton()
+        spin_clock_size.set_adjustment(spin_clock_size_adj)
+        spin_clock_size.set_digits(0)
+        self.attach(spin_clock_size, 1, 1, 1, 1)
 
         self.colorbuttons = []
 
@@ -104,18 +109,30 @@ class BudgieAnalogClockSettings(Gtk.Grid):
             self.attach(colorlabel, 0, n+2, 1, 1)
             self.attach(self.colorbuttons[n], 1, n+2, 1, 1)
 
-        self.label_reset = Gtk.Label("Reset clock face \nto transparent")
-        self.label_reset.set_halign(Gtk.Align.START)
-        self.button_reset_face = Gtk.Button("Reset")
-        self.button_reset_face.connect("clicked",self.on_reset_face)
-        self.attach(self.label_reset, 0, 5, 1, 1)
-        self.attach(self.button_reset_face, 1, 5, 1, 1)
+        label_reset = Gtk.Label("Reset clock face \nto transparent")
+        label_reset.set_halign(Gtk.Align.START)
+        button_reset_face = Gtk.Button("Reset")
+        button_reset_face.connect("clicked",self.on_reset_face)
+        self.attach(label_reset, 0, 5, 1, 1)
+        self.attach(button_reset_face, 1, 5, 1, 1)
 
-        app_settings.bind("clock-size",self.spin_clock_size,"value",Gio.SettingsBindFlags.DEFAULT)
+        blank_label_2 = Gtk.Label("")
+        self.attach(blank_label_2, 0, 6, 2, 1)
+
+        label_markings = Gtk.Label("Show hour marks")
+        label_markings.set_halign(Gtk.Align.START)
+        switch_markings = Gtk.Switch()
+        switch_markings.set_halign(Gtk.Align.END)
+        self.attach(label_markings, 0, 7, 1, 1)
+        self.attach(switch_markings, 1, 7, 1, 1)
+
+        app_settings.bind("clock-size",spin_clock_size,"value",Gio.SettingsBindFlags.DEFAULT)
+        app_settings.bind("draw-marks",switch_markings,"active",Gio.SettingsBindFlags.DEFAULT)        
 
         self.show_all()
 
     def on_reset_face(self, button):
+        self.colorbuttons[2].set_alpha(0)
         app_settings.set_string("clock-face","none")
 
     def on_color_changed (self, button, clock_part):
@@ -134,20 +151,20 @@ class BudgieAnalogClockApplet(Budgie.Applet):
 
         self.uuid = uuid
 
-        self.max_size = 100
-        self.clock_scale = 28
-
         user = os.environ["USER"]
         self.tmp = os.path.join("/tmp", user + "_panel_analog_clock.svg")
+
+        self.max_size = 100
+
+        self.validate_settings()
+        self.load_settings()
 
         self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.add(self.box)
         self.clock_image = Gtk.Image()
         self.box.add(self.clock_image)
-        self.validate_settings()
-        self.update_clock("","")
         self.show_all()
-        app_settings.connect("changed",self.update_clock)
+        app_settings.connect("changed",self.on_settings_change)
         GLib.timeout_add_seconds(UPDATE_INTERVAL, self.update_time)
 
     def do_panel_size_changed(self,panel_size,icon_size,small_icon_size):
@@ -160,10 +177,13 @@ class BudgieAnalogClockApplet(Budgie.Applet):
             app_settings.set_int("clock-size",22)
         elif current_size > self.max_size:
             self.clock_scale = self.max_size
-        self.update_clock("","")
+        self.update_clock()
 
     def validate_settings(self):
-        # Reset invalid colors to defaults - "none" is a valid color name
+        """ Reset invalid colors to defaults - "none" is a valid color name.
+            Invalid settings (hopefully) should really only ever occur if
+            the dconf values are manually set to something wrong
+        """
         setting_name = ["clock-hands", "clock-outline", "clock-face"]
         default_color = ["#000000", "#000000", "#FFFFFF"]
         for n in range(3):
@@ -172,15 +192,22 @@ class BudgieAnalogClockApplet(Budgie.Applet):
             if (colorname != "none") and (not testcolor.parse(colorname)):
                 app_settings.set_string(setting_name[n],default_color[n])
 
-    def update_clock(self,arg1,arg2):
-        self.old_minute = FORCE_UPDATE
-        self.validate_settings()
+    def on_settings_change(self, arg1, arg2):
+        self.load_settings()
+        self.update_clock()
+
+    def load_settings(self):
         self.clock_scale = app_settings.get_int("clock-size")
         if self.clock_scale > self.max_size:
             self.clock_scale = self.max_size
         self.hands_color = app_settings.get_string("clock-hands")
         self.line_color = app_settings.get_string("clock-outline")
         self.fill_color = app_settings.get_string("clock-face")
+        self.draw_hour_marks = app_settings.get_boolean("draw-marks")
+
+    def update_clock(self):
+        self.old_minute = FORCE_UPDATE
+        self.validate_settings()
         self.update_time()
 
     def update_time(self):
@@ -205,24 +232,26 @@ class BudgieAnalogClockApplet(Budgie.Applet):
 
         dwg = svgwrite.Drawing(self.tmp, (IMAGE_SIZE, IMAGE_SIZE))
         # Draw an outside circle for the clock, and a small circle at the base of the hands
-        dwg.add(dwg.circle((X_CENTER, Y_CENTER), CLOCK_RADIUS, 
-                           fill=self.fill_color, stroke=self.line_color, stroke_width=4))
+        dwg.add(dwg.circle((X_CENTER, Y_CENTER), CLOCK_RADIUS, fill=self.fill_color,
+                            stroke=self.line_color, stroke_width=CLOCK_THICKNESS))
         dwg.add(dwg.circle((X_CENTER, Y_CENTER), 3, 
                            stroke=self.hands_color, stroke_width=3))
 
-        # TODO: Maybe make the markings optional
         # We are going to add hour markings around the outside edge of the clock
-        for markings in range(12):
-            mark_rad = pi * 2 - (markings * (pi * 2) / 12)
-            mark_x = round (X_CENTER + (CLOCK_RADIUS - 3) * cos(mark_rad))
-            mark_y = round (X_CENTER + (CLOCK_RADIUS - 3)  * sin(mark_rad))
-            dwg.add(dwg.circle((mark_x,mark_y), 2, fill=self.line_color))
+        if self.draw_hour_marks:
+            for markings in range(12):
+                mark_rad = pi * 2 - (markings * (pi * 2) / 12)
+                mark_x = round (X_CENTER + (CLOCK_RADIUS - 3) * cos(mark_rad))
+                mark_y = round (X_CENTER + (CLOCK_RADIUS - 3)  * sin(mark_rad))
+                dwg.add(dwg.circle((mark_x,mark_y), 2, fill=self.line_color))
 
         # Draw the minute and hour hands from the center to the calculated points
         hour_hand_x, hour_hand_y = self.get_clock_hand_xy (hours, HOUR_HAND_LENGTH)
         minute_hand_x, minute_hand_y = self.get_clock_hand_xy (mins, MINUTE_HAND_LENGTH)
-        dwg.add(dwg.line((X_CENTER,Y_CENTER), (hour_hand_x,hour_hand_y), stroke=self.hands_color, stroke_width=6))
-        dwg.add(dwg.line((X_CENTER,Y_CENTER), (minute_hand_x,minute_hand_y), stroke=self.hands_color, stroke_width=6))
+        dwg.add(dwg.line((X_CENTER,Y_CENTER), (hour_hand_x,hour_hand_y),
+                          stroke=self.hands_color, stroke_width=HAND_THICKNESS))
+        dwg.add(dwg.line((X_CENTER,Y_CENTER), (minute_hand_x,minute_hand_y),
+                          stroke=self.hands_color, stroke_width=HAND_THICKNESS))
         dwg.save()
 
     def get_clock_hand_xy (self, hand_position, LENGTH):
