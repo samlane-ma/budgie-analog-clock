4b25ff0f541f8f1d7eeebada2d0172f6f32f3166
Sam Lane <67085765+samlane-ma@users.noreply.github.com>
2020-09-13 18:48:04 -0400

    Remove unnecessary data from svg creator

--- src/createsvg.py
+++ src/createsvg.py
@@ -63,9 +63,5 @@ class createSVG:
         self.filename = filename
 
     def set_size(self, x, y):
-        self.svgsize =  ('<svg baseProfile="full" height="'+ str(y) +
-                        '" version="1.1" width="' + str(x) +
-                        '" xmlns="http://www.w3.org/2000/svg"' +
-                        ' xmlns:ev="http://www.w3.org/2001/xml-events"' +
-                        ' xmlns:xlink="http://www.w3.org/1999/xlink"><defs />')
+        self.svgsize =  ('<svg height="'+ str(y) + '" width="' + str(x) + '">')
 
